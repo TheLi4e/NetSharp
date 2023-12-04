99c5f1611edfb472876e042b5f2ec74515bc43a0
TheLi4e <li4e99@gmail.com>
2023-12-04 17:35:51 +0200

    Lesson2

---
+++ Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using NetSharp;
+using System.Text;
+using System.Net.Security;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        UDPServer.Server("Hello");
+    }
+  
+}
