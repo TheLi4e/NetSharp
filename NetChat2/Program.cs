41c1dd3a2974c92c1adaa7edd742fb640ac65f14
TheLi4e <li4e99@gmail.com>
2023-12-19 14:37:56 +0200

    HW5-6

---
+++ NetChat2/Program.cs
@@ -0,0 +1,22 @@
+﻿using NetChat2.Services;
+
+namespace NetChat2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args[0].Equals("Server"))
+            {
+                var netmsgsorce = new UdpMessageSouce();
+                var server = new Server(netmsgsorce);
+                await server.Start();
+            }
+            else
+            {
+                var client = new Client("Vasya", "172.0.0.1", 12345);
+                await client.Start();
+            }
+        }
+    }
+}
